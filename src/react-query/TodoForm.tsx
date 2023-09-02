a3b7cc415e594672f20c3fda55d38fced46c1de7
Paul Otieno <vitalispaul48@live.com>
2023-09-02 11:53:36 +0300

    Add  loading state

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -23,6 +23,8 @@ const TodoForm = () => {
         savedTodo,
         ...(todos || []),
       ]);
+
+      if (ref.current) ref.current.value = "";
     },
   });
   const ref = useRef<HTMLInputElement>(null);
@@ -51,7 +53,9 @@ const TodoForm = () => {
           <input ref={ref} type="text" className="form-control" />
         </div>
         <div className="col">
-          <button className="btn btn-primary">Add</button>
+          <button className="btn btn-primary" disabled={addTodo.isLoading}>
+            {addTodo.isLoading ? "Adding..." : "Add"}
+          </button>
         </div>
       </form>
     </>
