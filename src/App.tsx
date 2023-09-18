a83a802b51ad35e55ea6a8b677be94f5f6973672
Paul Otieno <vitalispaul48@live.com>
2023-09-18 07:21:55 +0300

    Add counterReducer

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import "./App.css";
 import PostList from "./react-query/PostList";
 import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
+import Counter from "./state-management/Counter";
 
 function App() {
   return (
     <>
       {/* <TodoList /> */}
-      <TodoForm />
+      {/* <TodoForm /> */}
       {/* <PostList /> */}
-      <TodoList />
+      {/* <TodoList /> */}
+      <Counter />
     </>
   );
 }
--- src/state-management/Counter.tsx
+++ src/state-management/Counter.tsx
@@ -1,19 +1,23 @@
-import { useState } from 'react';
+import { useReducer, useState } from "react";
+import counterReducer from "./reducers/counterReducer";
 
 const Counter = () => {
-  const [value, setValue] = useState(0);
+  // First argument is our reducer function
+  // Second argument is our initial state
+  // Returns array with two  ellements. current state and function for triggering changes dispatch
+  const [value, dispatch] = useReducer(counterReducer, 0);
 
   return (
     <div>
       Counter ({value})
       <button
-        onClick={() => setValue(value + 1)}
+        onClick={() => dispatch({ type: "INCREMENT" })}
         className="btn btn-primary mx-1"
       >
         Increment
       </button>
       <button
-        onClick={() => setValue(0)}
+        onClick={() => dispatch({ type: "RESET" })}
         className="btn btn-primary mx-1"
       >
         Reset
