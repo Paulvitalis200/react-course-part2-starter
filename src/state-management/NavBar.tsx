40b1da9931a994859aeb18cda3ec3ed3110dbc0a
Paul Otieno <vitalispaul48@live.com>
2023-09-25 06:47:21 +0300

    Install simple-zustand devtools

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -2,7 +2,9 @@ import LoginStatus from "./auth/LoginStatus";
 import useCounterStore from "./counter/store";
 
 const NavBar = () => {
-  const { counter } = useCounterStore();
+  const counter = useCounterStore(s => s.counter);
+
+  console.log("Render Navbar")
   return (
     <nav className="navbar d-flex justify-content-between">
       <span className="badge text-bg-secondary">{counter}</span>
--- src/state-management/counter/store.ts
+++ src/state-management/counter/store.ts
@@ -1,18 +1,24 @@
 // Create a  store for managing state of our counter
 
 import { create } from "zustand";
+import { mountStoreDevtool } from "simple-zustand-devtools";
 
 // Shape of store
 interface CounterStore {
   counter: number;
+  max:number;
   increment: () => void;
   reset: () => void;
 }
 
 const useCounterStore = create<CounterStore>((set) => ({
   counter: 0,
+  max: 5,
   increment: () => set((store) => ({ counter: store.counter + 1 })),
-  reset: () => set(() => ({ counter: 0 })),
+  reset: () => set(() => ({ max: 10 })),
 }));
+
+if (process.env.NODE_ENV === 'development') 
+mountStoreDevtool('Counter Store', useCounterStore)
 
 export default useCounterStore;
